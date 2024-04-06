dfe9920aa9711d61794f7564230526761c94bec7
upendar thota <e213079@chaitanya.edu.in>
2024-04-07 02:57:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,97 +1,75 @@
 // import logo from './logo.svg';
-import './App.css';
-import * as React from 'react';
-import Navbar from './components/Navbar';
-import TeForm from './components/TeForm';
-import About from './components/About';
-import Alert from './components/Alert';
-import { useState } from 'react';
-import { BrowserRouter, Routes,Route } from "react-router-dom";
- 
- 
-    function App() {
-      const[mode,setMode]=useState('light') //weather dark mode is enable or not
+import "./App.css";
+import * as React from "react";
+import Navbar from "./components/Navbar";
+import TeForm from "./components/TeForm";
+// import About from "./components/About";
+import Alert from "./components/Alert";
+import { useState } from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-      const [alert,setAlert]=useState(null)
-      const showAlert=(message,type)=>{
-        setAlert({
-          msg:message,
-        type:type        })
+function App() {
+  const [mode, setMode] = useState("light"); //weather dark mode is enable or not
 
-        setTimeout( ()=>{
-        setAlert(null)
-         },3000)
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
 
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  };
 
-      }
+  const removeBodyClasses = () => {
+    document.body.classList.remove("bg-light");
+    document.body.classList.remove("bg-dark");
+    document.body.classList.remove("bg-warning");
+    document.body.classList.remove("bg-danger");
+    document.body.classList.remove("bg-success");
+  };
 
-const removeBodyClasses=()=>{
-  document.body.classList.remove('bg-light')
-  document.body.classList.remove('bg-dark')
-  document.body.classList.remove('bg-warning')
-  document.body.classList.remove('bg-danger')
-  document.body.classList.remove('bg-success')
-   
-
-
-}
-
-      const toggleMode = (cls) => {
-removeBodyClasses()
-        document.body.classList.add('bg-'+cls)
-        if(mode === 'light'){
-          setMode('dark')
-          document.body.style.backgroundColor='#042743'
-          showAlert("Dark mode enables","success")
-          // document.title='TextUtils-Dark mode';
-
-          // setInterval(() => {
-          //   document.title="TextUtils is  Amazing Mode"
-          // }, 2000);
-          // setInterval(() => {
-          //   document.title="Install TextUtils Now"
-          // }, 1000);
-
-
-
-        }
-        else{
-          setMode('light')
-          document.body.style.backgroundColor='white'
-          showAlert("light mode enables","success")
-          // document.write='TextUtils-lightMode'
-        }
-      }
-
-
-      return (
-        <> 
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-    <Alert alert={alert}/>
-       <div className="class container my-3"> 
-      <BrowserRouter>
-           <Routes>
-
-                 <Route path="/" />
-                <Route path="/teform" element={<TeForm  showAlert={showAlert} heading=" Try-TetUtils --Word counter and character counter and remove etra spaces " mode={mode}/> }/>
-
-                <Route path="/about" element={<About mode={mode}/>} /> 
-            </Routes>
-
-
-      </BrowserRouter>
-
-
-        {/* <TeForm  showAlert={showAlert} heading="Enter the  tet to analyze " mode={mode}/> */}
-        {/* <About/> */}
-        </div>
-       
-
-        
-     </>
-     
-       );
+  const toggleMode = (cls) => {
+    removeBodyClasses();
+    document.body.classList.add("bg-" + cls);
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode enables", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("light mode enables", "success");
     }
- 
+  };
+
+  return (
+    <>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="class container my-3">
+        {/* <BrowserRouter>
+          <Routes>
+            <Route path="/" /> */}
+            {/* <Route
+              path="/teform"
+              element={ */}
+                <TeForm
+                  showAlert={showAlert}
+                  heading=" Try-TetUtils --Word counter and character counter and remove etra spaces "
+                  mode={mode}
+                />
+            {/* //   
+            // /> */}
+
+            {/* <Route path="/about" element={<About mode={mode} />} /> */}
+          {/* </Routes>
+        </BrowserRouter> */}
+      </div>
+    </>
+  );
+}
 
 export default App;
